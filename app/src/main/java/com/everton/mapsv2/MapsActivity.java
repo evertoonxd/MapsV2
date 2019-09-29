41b8015d00e1da2d8bf500b0b925a6d2e3dea98d
Everton França <evertoonxd@gmail.com>
2019-09-29 13:50:49 -0300

    start commit

---
+++ app/src/main/java/com/everton/mapsv2/MapsActivity.java
@@ -0,0 +1,279 @@
+package com.everton.mapsv2;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.fragment.app.FragmentActivity;
+
+import android.app.AlertDialog;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Handler;
+import android.provider.Settings;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.ArrayList;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, LocationListener {
+
+    private GoogleMap mMap;
+
+    Marker currentLocationMarker, petLocationMarker;
+    LatLng currentLocationLatLong, petLocation;
+    double boyLat, boyLng;
+    double petLat = -9.760557;
+    double petLng = -36.653340;
+    double distPet = 50.0;
+
+    private static Uri alarmSound;
+    private final long[] pattern = {100, 300, 300, 300}; //used to vibrate notication
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        startGettingLocations();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        if (currentLocationMarker != null) {
+            currentLocationMarker.remove();
+        }
+        //add marker boy
+        mMap.clear();
+        boyLat = location.getLatitude();
+        boyLng = location.getLongitude();
+        currentLocationLatLong = new LatLng(boyLat, boyLng);
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(currentLocationLatLong);
+        markerOptions.title("Você");
+        //markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE));
+        markerOptions.icon(BitmapDescriptorFactory.fromResource(R.drawable.boyx24));
+        currentLocationMarker = mMap.addMarker(markerOptions);
+
+        // Add marker pet
+        petLocation = new LatLng(petLat, petLng);
+
+        MarkerOptions markerOptionsPet = new MarkerOptions();
+        markerOptionsPet.position(petLocation);
+        markerOptionsPet.title("PET");
+        markerOptionsPet.icon(BitmapDescriptorFactory.fromResource(R.drawable.pawprint));
+        markerOptionsPet.draggable(true);
+
+        float results [] = new float[10]; // guarda distancia
+        //calcula distancia entre dois pontos
+        Location.distanceBetween(boyLat, boyLng, petLat, petLng, results);
+        String resS = String.format("%.2f",results[0]);
+        float resFloat = results[0];
+
+        if(resFloat>distPet){
+            //inserir circulo
+            Circle circle = mMap.addCircle(new CircleOptions()
+                    .center(petLocation)
+                    .radius(distPet)
+                    .strokeColor(Color.rgb(245,181,76))
+                    .strokeWidth(5)
+                    .fillColor(Color.rgb(175,248,76)));
+            markerOptionsPet.snippet("Distancia = "+resS+"m");
+            petLocationMarker = mMap.addMarker(markerOptionsPet);
+
+
+
+            //create notification
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(MapsActivity.this)
+                    .setSmallIcon(R.mipmap.ic_launcher_round)
+                    .setContentTitle("PetTracker - Everton França")
+                    .setContentText("Seu Pet está a mais de " + distPet + "m")
+                    .setSound(alarmSound)
+                    .setVibrate(pattern);
+
+            //create sound notification
+            //default alarm sound
+            alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+            //Create intent
+            Intent notivationIntent = new Intent(MapsActivity.this, MapsActivity.class);
+            PendingIntent contentIntent = PendingIntent.getActivity(MapsActivity.this, 0, notivationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+            builder.setContentIntent(contentIntent);
+
+            //add notivication
+            NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            manager.notify(0, builder.build());
+
+        }else{
+            markerOptionsPet.snippet("Distancia = " + resS + "m");
+            petLocationMarker = mMap.addMarker(markerOptionsPet);
+        }
+        //move camera location
+        CameraPosition cameraPosition = new CameraPosition.Builder().zoom(19).target(currentLocationLatLong).build();
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+
+        Toast.makeText(this, "Localização atualizada", Toast.LENGTH_SHORT).show();
+    }
+
+    //permissões
+    private boolean hasPermission(String permission) {
+        if (canAskPermission()) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                return (checkSelfPermission(permission) == PackageManager.PERMISSION_GRANTED);
+            }
+        }
+        return true;
+    }
+
+    private boolean canAskPermission() {
+        return (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP_MR1);
+    }
+
+    public void showSettingsAlert() {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+        alertDialog.setTitle("GPS desativado!");
+        alertDialog.setMessage("Ativar GPS?");
+        alertDialog.setPositiveButton("Sim", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(intent);
+            }
+        });
+
+        alertDialog.setNegativeButton("Não", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        alertDialog.show();
+    }
+
+
+    private void startGettingLocations() {
+
+        LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        boolean isGPS = lm.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        boolean isNetwork = lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+        boolean canGetLocation = true;
+        int ALL_PERMISSIONS_RESULT = 101;
+        long MIN_DISTANCE_CHANGE_FOR_UPDATES = 1;// Distance in meters
+        long MIN_TIME_BW_UPDATES = 1000 * 10;// Time in milliseconds
+
+        ArrayList<String> permissions = new ArrayList<>();
+        ArrayList<String> permissionsToRequest;
+
+        permissions.add(android.Manifest.permission.ACCESS_FINE_LOCATION);
+        permissions.add(android.Manifest.permission.ACCESS_COARSE_LOCATION);
+        permissionsToRequest = findUnAskedPermissions(permissions);
+
+        //Check if GPS and Network are on, if not asks the user to turn on
+        if (!isGPS && !isNetwork) {
+            showSettingsAlert();
+        } else {
+            // check permissions
+
+            // check permissions for later versions
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (permissionsToRequest.size() > 0) {
+                    requestPermissions(permissionsToRequest.toArray(new String[permissionsToRequest.size()]),
+                            ALL_PERMISSIONS_RESULT);
+                    canGetLocation = false;
+                }
+            }
+        }
+
+
+        //Checks if FINE LOCATION and COARSE Location were granted
+        if (ActivityCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED) {
+
+            Toast.makeText(this, "Permissão negada", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        //Starts requesting location updates
+        if (canGetLocation) {
+            if (isGPS) {
+                lm.requestLocationUpdates(
+                        LocationManager.GPS_PROVIDER,
+                        MIN_TIME_BW_UPDATES,
+                        MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+
+            } else if (isNetwork) {
+                // from Network Provider
+
+                lm.requestLocationUpdates(
+                        LocationManager.NETWORK_PROVIDER,
+                        MIN_TIME_BW_UPDATES,
+                        MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+
+            }
+        } else {
+            Toast.makeText(this, "Não é possível obter a localização", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private ArrayList<String> findUnAskedPermissions(ArrayList<String> wanted) {
+        ArrayList result = new ArrayList();
+
+        for (String perm : wanted) {
+            if (!hasPermission(perm)) {
+                result.add(perm);
+            }
+        }
+
+        return result;
+    }
+
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+}
